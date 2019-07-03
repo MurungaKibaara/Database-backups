caa8bf71c58b86d5bfc2637b64f579e7d23d49ff
MurungaKibaara <murungakibaara@outlook.com>
2019-07-03 15:52:37 +0300

    [Added better functionality for removing old backup files]

--- create_dump.py
+++ create_dump.py
@@ -8,12 +8,9 @@ from datetime import datetime
 # Change environment variable to powershell on windows
 
 os.environ['COMSPEC'] = 'powershell'
-<<<<<<< HEAD
-=======
 password = 'smart'
 user = 'root'
 database = 'tracking'
->>>>>>> dc16f00df91d7a1e4f784f68da7f6d5828e021da
 
 def count():
 	date_time = datetime.now()
@@ -25,7 +22,6 @@ def backup():
 	today = count()
 	backup_name = "backup"+today+".sql"
 
-<<<<<<< HEAD
 	try:
 		with open(backup_name, 'w') as backup_file:
 			password = 'smart'
@@ -51,8 +47,6 @@ def backup():
 
 	except:
 		print('failed to create file')
-=======
-	# with open(backup_name) as backup_file:
-	process = Popen([r'mysqldump', '-u', 'root', '-psmart', 'tracking', 'tbl_cars','cars_status'], stdin=PIPE, stderr=PIPE, stdout=open(backup_name, 'w+'), shell=True)
-	process.communicate()
->>>>>>> dc16f00df91d7a1e4f784f68da7f6d5828e021da
+	# # with open(backup_name) as backup_file:
+	# process = Popen([r'mysqldump', '-u', 'root', '-psmart', 'tracking', 'tbl_cars','cars_status'], stdin=PIPE, stderr=PIPE, stdout=open(backup_name, 'w+'), shell=True)
+	# process.communicate()
--- delete_old_files.py
+++ delete_old_files.py
@@ -1,15 +1,47 @@
-'''Delete old logs'''
-
+'''Delete old backup files'''
+from datetime import datetime, timedelta
+import datefinder
 import os
+import re
 from glob import glob
 from contextlib import suppress
 
+def format():
+    time = datetime.now().strftime("%Y-%m-%d")
+    return time
+
 def delete_backups():
     '''Delete all logs with .sql name'''
+    backup_date = format()
+
     with suppress(OSError):
         try:
             for files in glob('*.sql'):
-                os.remove(files)
+                with open(files, 'r') as bfile:
+                    bfile_name = os.path.basename(bfile.name)
+
+                    try:
+                        dates = list(datefinder.find_dates(bfile_name))
+                        if dates is not None:
+                            for date in dates:
+                                new_date = date.strftime("%Y-%m-%d")
+
+                                if new_date < backup_date:
+                                    try:
+                                        os.remove(files)
+                                        print('Removed past files only')
+                                    except:
+                                        print('Error removing files')
+                                else:
+                                    print('Files can only be deleted after 48 hours')
+                        else:
+                            print('No backups from 2 days ago')
+                    except:
+                        print('File doesnt have that group')
         except Exception as error:
             print('Error occured: ', error)
+
+# Unused Important Code
+# match = re.search(r'\d{4}-\d{2}-\d{2}', bfile_name)
+# date = datetime.strptime(match.group(), '-%Y-%m-%d-%H-%M-%S').date()
 
