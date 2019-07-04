e4e54ac15ccdb3eec2c0592c077c3dfd43ca078e
Evans <murungakibaara@outlook.com>
2019-07-04 15:31:56 +0300

    [Modified create dump file to work on windows systems]

--- create_dump.py
+++ create_dump.py
@@ -5,14 +5,6 @@ from subprocess import PIPE, call, Popen
 import subprocess
 from datetime import datetime
 
-# Change environment variable to powershell on windows
-
-# os.environ['COMSPEC'] = 'powershell'
-
-password = ''
-user = 'root'
-
-
 def count():
 	date_time = datetime.now()
 	today = date_time.strftime("%m-%d-%Y-%H-%M-%S")
@@ -22,7 +14,6 @@ def backup(db_name):
 	'''Backup to a *.sql file'''
 	today = count()
 	backup_name = "backup"+today+".sql"
-
 	try:
 		process = Popen([r'mysqldump', '-u', 'root', '-psmart', 'tracking', 'tbl_cars','cars_status'], stdin=PIPE, stderr=PIPE, stdout=open(backup_name, 'w+'), shell=True)
 		process.communicate()
